315923fee4e915c3e27227de5f6135bf4bc1e91e
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-10-21 17:00:19 +0300

    Docs: added swagger for better documentation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './modules/common/filters/all-exceptions.filter';
+import { ResponseInterceptor } from './modules/common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('HagzNow API')
+    .setDescription('API for reserving sports arenas')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      // transform: true,
+      transform: true,
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
