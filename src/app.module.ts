daf0f2bf12ba1d6d8633a7cb61073d3e91ea9d16
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-10-29 21:11:56 +0300

    Feat: added authorization guard & decorator using role based access

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import { DatabaseModule } from './database/database.module';
@@ -7,7 +7,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 // import { databaseProviders } from './database/database.providers';
+import { APP_GUARD } from '@nestjs/core';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { RolesGuard } from './common/guards/roles.guard';
 import { ArenasModule } from './modules/arenas/arenas.module';
 import { ArenaExtra } from './modules/arenas/entities/arena-extra.entity';
 import { ArenaImage } from './modules/arenas/entities/arena-image.entity';
@@ -19,6 +21,7 @@ import { Category } from './modules/categories/entities/category.entity';
 import { Reservation } from './modules/reservations/entities/reservation.entity';
 import { ReservationsModule } from './modules/reservations/reservations.module';
 import { User } from './modules/users/entities/user.entity';
+import { CurrentUserMiddleware } from './modules/users/middlewares/current-user.middleware';
 import { WalletTransaction } from './modules/wallets/entities/wallet-transaction.entity';
 import { Wallet } from './modules/wallets/entities/wallet.entity';
 import { WalletModule } from './modules/wallets/wallets.module';
@@ -63,6 +66,16 @@ import { WalletModule } from './modules/wallets/wallets.module';
     ReservationsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,42 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { ApiResponseUtil } from '../utils/api-response.util';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles || requiredRoles.length === 0) return true;
+
+    const { user } = context.switchToHttp().getRequest();
+
+    if (!user || !user.role)
+      return ApiResponseUtil.throwError(
+        'You do not have permission',
+        'NOT_ALLOWED',
+        HttpStatus.FORBIDDEN,
+      );
+
+    const hasRole = requiredRoles.includes(user.role);
+    if (!hasRole)
+      return ApiResponseUtil.throwError(
+        'You do not have permission',
+        'NOT_ALLOWED',
+        HttpStatus.FORBIDDEN,
+      );
+
+    return true;
+  }
+}
