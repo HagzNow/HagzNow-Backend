3b9755042a2b7beeeb7fdab8d0ae2792814c9b5e
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-10-29 21:11:12 +0300

    Feat: added current user middleware & decorator

---
+++ src/modules/users/middlewares/current-user.middleware.ts
@@ -0,0 +1,38 @@
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware {
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) return next(); // no token, just continue
+
+    const [type, token] = authHeader.split(' ');
+    if (type !== 'Bearer' || !token) return next();
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get<string>('JWT_SECRET'),
+      });
+
+      // attach user payload to request
+      req['user'] = payload;
+    } catch (err) {
+      // you can choose to throw or ignore
+      throw new UnauthorizedException('Invalid or expired token');
+    }
+
+    next();
+  }
+}
