336b03d24c701878f0f2458fcf76a4fd618de8e8
Mohammed Sersawy <mohammed@sersawy.com>
2025-11-16 21:26:17 +0200

    Fix: implement getDashboardData method to aggregate arena statistics for owners

--- src/modules/owners/owners.controller.ts
+++ src/modules/owners/owners.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { OwnersService } from './owners.service';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { User } from '../users/entities/user.entity';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from '../users/interfaces/userRole.interface';
 
-@Controller('controller')
-export class OwnersController {}
+@Controller('owner')
+export class OwnersController {
+  constructor(private readonly ownersService: OwnersService) {}
+
+  @Roles(UserRole.OWNER)
+  @Get('dashboard')
+  async getDashboardData(@CurrentUser() user: User) {
+    return await this.ownersService.getDashboardData(user.id);
+  }
+}
--- src/modules/owners/owners.service.ts
+++ src/modules/owners/owners.service.ts
@@ -4,13 +4,52 @@ import { Repository } from 'typeorm';
 import { User } from '../users/entities/user.entity';
 import { UsersService } from '../users/users.service';
 import { Injectable } from '@nestjs/common';
+import { ArenasService } from '../arenas/arenas.service';
+import { ReservationsService } from '../reservations/reservations.service';
+import { WalletTransactionService } from '../wallets/wallet-transaction.service';
+import { ArenaSlotsService } from '../arenas/arena-slots.service';
 
 @Injectable()
 export class OwnersService extends UsersService {
   constructor(
     eventEmitter: EventEmitter2,
     @InjectRepository(User) userRepository: Repository<User>,
+    private readonly arenasService: ArenasService,
+    private readonly reservationsService: ReservationsService,
+    private readonly walletTransactionService: WalletTransactionService,
+    private readonly ArenaSlotsService: ArenaSlotsService,
   ) {
     super(eventEmitter, userRepository);
+  }
+
+  async getDashboardData(ownerId: string) {
+    const totalArenas =
+      await this.arenasService.getNumberOfArenasByOwner(ownerId);
+
+    const totalReservations =
+      await this.reservationsService.getNumberOfReservationsByOwner(ownerId);
+
+    const totalEarnings =
+      await this.walletTransactionService.getTotalTransactionsAmountByUserId(
+        ownerId,
+      );
+
+    const occupancyRate =
+      await this.ArenaSlotsService.getOccupancyRate(ownerId);
+
+    const topArena =
+      await this.arenasService.getMostReservedArenaByOwner(ownerId);
+
+    const popularTime =
+      await this.ArenaSlotsService.getPopularBookingTimes(ownerId);
+
+    return {
+      totalArenas,
+      totalReservations,
+      totalEarnings,
+      occupancyRate,
+      topArena,
+      popularTime,
+    };
   }
 }
